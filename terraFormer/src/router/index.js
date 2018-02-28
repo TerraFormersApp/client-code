73540cdfe97f9119d8ac2756d138326130601087
Patrick Biffle <pbiffle@gmail.com>
2018-02-28 16:55:53 -0700

    added gif to files

--- terraFormer/src/router/index.js
+++ terraFormer/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LandingPage from '@/components/LandingPage'
+import Editor from '@/components/Editor'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'LandingPage',
+    //   component: LandingPage
+    // },
     {
       path: '/',
-      name: 'LandingPage',
-      component: LandingPage
+      name: 'Editor',
+      component: Editor
     }
   ]
 })
