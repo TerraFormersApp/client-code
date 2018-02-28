250ceb1ee505891d4b1bc11db9b66edb782e82d1
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-02-28 16:54:27 -0700

    fixed checkboxes, I hope

--- terraFormer/src/components/Editor.vue
+++ terraFormer/src/components/Editor.vue
@@ -54,21 +54,45 @@
             <span class="custom-file-control"></span>
             </label>
         </div>
-        <div class="custom-control custom-checkbox">
+        <div class="form-group">
+            <div class="custom-control custom-checkbox">
             <input class="custom-control-input" id="customCheck1" checked="" type="checkbox">
             <label class="custom-control-label" for="customCheck1">Mountains</label>
+            </div>
+            <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" id="customCheck2" disabled="" type="checkbox">
+            <label class="custom-control-label" for="customCheck2">Mountains</label>
+            </div>
         </div>
-        <div class="custom-control custom-checkbox">
+        <div class="form-group">
+            <div class="custom-control custom-checkbox">
             <input class="custom-control-input" id="customCheck1" checked="" type="checkbox">
             <label class="custom-control-label" for="customCheck1">Trees</label>
+            </div>
+            <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" id="customCheck2" disabled="" type="checkbox">
+            <label class="custom-control-label" for="customCheck2">Trees</label>
+            </div>
         </div>
-        <div class="custom-control custom-checkbox">
+        <div class="form-group">
+            <div class="custom-control custom-checkbox">
             <input class="custom-control-input" id="customCheck1" checked="" type="checkbox">
             <label class="custom-control-label" for="customCheck1">Wind</label>
+            </div>
+            <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" id="customCheck2" disabled="" type="checkbox">
+            <label class="custom-control-label" for="customCheck2">Wind</label>
+            </div>
         </div>
-        <div class="custom-control custom-checkbox">
+        <div class="form-group">
+            <div class="custom-control custom-checkbox">
             <input class="custom-control-input" id="customCheck1" checked="" type="checkbox">
             <label class="custom-control-label" for="customCheck1">Hurricanes</label>
+            </div>
+            <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" id="customCheck2" disabled="" type="checkbox">
+            <label class="custom-control-label" for="customCheck2">Hurricanes</label>
+            </div>
         </div>
     </form>
   </div>
