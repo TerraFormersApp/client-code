798b78cd86e2f767d2010379c6d20d2e3593af9e
Patrick Biffle <pbiffle@gmail.com>
2018-02-28 17:07:56 -0700

    changed routing

--- terraFormer/src/router/index.js
+++ terraFormer/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LandingPage from '@/components/LandingPage'
+import Editor from '@/components/Editor'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'LandingPage',
       component: LandingPage
+    },
+    {
+      path: '/editor',
+      name: 'Editor',
+      component: Editor
     }
   ]
 })
