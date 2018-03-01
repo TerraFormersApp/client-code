37df3de7e8bd6cb1bdfd7d490451a3e1aad1f9c4
Patrick Biffle <pbiffle@gmail.com>
2018-02-28 17:14:09 -0700

    added bad landing page

--- terraFormer/src/components/LandingPage.vue
+++ terraFormer/src/components/LandingPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='page-landing'>
     <h1>IM A LANDING PAGE</h1>
+    <img src="../../static/Comp-1_1.gif" alt="hold">
   </div>
 </template>
 <script>
@@ -13,4 +14,10 @@ export default {
   }
 }</script>
 <style scoped>
+.page-landing{
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
 </style>
