681469d9d1796a06a0ca36fe95782d13137ec158
Patrick Biffle <pbiffle@gmail.com>
2018-02-28 17:15:20 -0700

    added router-link

--- terraFormer/src/components/LandingPage.vue
+++ terraFormer/src/components/LandingPage.vue
@@ -1,7 +1,9 @@
 <template>
   <div class='page-landing'>
     <h1>IM A LANDING PAGE</h1>
-    <img src="../../static/Comp-1_1.gif" alt="hold">
+    <router-link to="/editor">
+      <img src="../../static/Comp-1_1.gif" alt="hold">
+    </router-link>
   </div>
 </template>
 <script>
