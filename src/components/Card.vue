8f10f2715ab0269389708b6a6a7820f5a39b41f8
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-03-03 12:27:10 -0700

    delete and update work now

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,6 +13,8 @@
     		<p class="card-text">Wind: {{data.wind}}</p>
     		<p class="card-text">Mountains: {{data.mountains}}</p>
     		<p class="card-text">Trees: {{data.trees}}</p>
+				<button @click="updatePlanet" type="button" class="btn btn-secondary">Edit Planet</button>
+				<button @click="deletePlanet" type="button" class="btn btn-danger">Delete Planet</button>
   		</div>
 		</div>
   </div>
@@ -21,16 +23,42 @@
 <script>
 export default {
   name: "Card",
-	props: ["data"]
-}
+  props: ["data", "fetchPlanets"],
+  data() {
+    return {
+      submission: { name: "" }
+    };
+  },
+  methods: {
+    deletePlanet() {
+      fetch("http://localhost:3000/planets/" + this.data.id, {
+        method: "DELETE"
+      }).then(() => this.fetchPlanets());
+    },
+    updatePlanet() {
+      let nameChange = prompt("Rename the planet here:", "");
+      if (nameChange != null) {
+        this.submission.name = nameChange;
+        console.log(this.submission.name);
+        fetch("http://localhost:3000/planets/" + this.data.id, {
+          method: "put",
+          headers: new Headers({ "Content-Type": "application/json" }),
+          body: JSON.stringify(this.submission)
+        })
+          .then(() => console.log("Name changed!"))
+          .then(() => this.fetchPlanets());
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
 #Card {
-	padding: 5px 10px 5px 10px;
+  padding: 5px 10px 5px 10px;
 }
 
 img {
-	height: 30px
+  height: 30px;
 }
 </style>
