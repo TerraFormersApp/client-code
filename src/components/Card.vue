2baeb441f8404213980ccf31d64136d246457fff
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-03-03 18:02:32 -0700

    that is pretty cool...

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="Card">
+  <div id="Card" :class="{shakeable}">
 		<div class="card border-info mb-3" style="width: 20rem;">
   		<div class="card-header">
 				<h3>{{data.name}} <img src="../../static/images/AcidPlanet.gif"></h3>
@@ -8,7 +8,7 @@
     		<h5 class="card-title">Planet Description:</h5>
     		<p>{{data.planet_description}}</p>
 				<button @click="updatePlanet" type="button" class="btn btn-secondary">Rename Your Planet</button>
-				<button @click="deletePlanet" type="button" class="btn btn-danger">DeathStar Destroy</button>
+				<button @click="destroy" type="button" class="btn btn-danger">DeathStar Destroy</button>
   		</div>
 		</div>
   </div>
@@ -20,7 +20,8 @@ export default {
   props: ["data", "fetchPlanets"],
   data() {
     return {
-      submission: { name: "" }
+      submission: { name: "" },
+      shakeable: false
     };
   },
   methods: {
@@ -30,14 +31,18 @@ export default {
       );
       audio.play();
     },
+    shakeIt() {
+      this.shakeable = true;
+    },
+    destroy() {
+      this.playSound();
+      setTimeout(this.shakeIt, 5000);
+      setTimeout(this.deletePlanet, 9000);
+    },
     deletePlanet() {
       fetch("http://localhost:3000/planets/" + this.data.id, {
         method: "DELETE"
-      })
-        .then(() => {
-          this.playSound();
-        })
-        .then(() => this.fetchPlanets());
+      }).then(() => this.fetchPlanets());
     },
     updatePlanet() {
       let nameChange = prompt("Rename the planet here:", "");
@@ -67,5 +72,46 @@ img {
 }
 button {
   width: 15rem;
+}
+.shakeable {
+  /* Start the shake animation and make the animation last for 0.5 seconds */
+  animation: shake 4s;
+  /* When the animation is finished, start again */
+  animation-iteration-count: infinite;
+}
+@keyframes shake {
+  0% {
+    transform: translate(1px, 1px) rotate(0deg);
+  }
+  10% {
+    transform: translate(-1px, -2px) rotate(-1deg);
+  }
+  20% {
+    transform: translate(-3px, 0px) rotate(1deg);
+  }
+  30% {
+    transform: translate(3px, 2px) rotate(0deg);
+  }
+  40% {
+    transform: translate(1px, -1px) rotate(1deg);
+  }
+  50% {
+    transform: translate(-1px, 2px) rotate(-1deg);
+  }
+  60% {
+    transform: translate(-3px, 1px) rotate(0deg);
+  }
+  70% {
+    transform: translate(3px, 1px) rotate(-1deg);
+  }
+  80% {
+    transform: translate(-1px, -1px) rotate(1deg);
+  }
+  90% {
+    transform: translate(1px, 2px) rotate(0deg);
+  }
+  100% {
+    transform: translate(1px, -2px) rotate(-1deg);
+  }
 }
 </style>
